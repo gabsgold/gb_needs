4e8fb03cfd91cf07133c03829ea571e15359c6f8
gabsgold <gabsgold1@yahoo.fr>
2017-04-29 16:52:20 +0200

    En cours...

---
+++ gb_needs/__resource.lua
@@ -0,0 +1,10 @@
+resource_manifest_version '77731fab-63ca-442c-a67b-abc70f28dfa5'
+
+ui_page 'food.html'
+files {
+	'food.html',
+	'pdown.ttf'
+}
+
+client_script "client.lua"
+server_script "server.lua"
